ed020ae356b9fc295a64f7aa5080cdd6ecc81626
christopher <post@c-ha.de>
2018-06-22 10:55:47 +0200

    handle output errors properly, fix issue #15

--- printers/printer.go
+++ printers/printer.go
@@ -13,13 +13,19 @@ var file *os.File
 
 func outputOpen() {
 	if config.Options.Output == "tcp" {
-		tcpConn, _ = net.Dial("tcp", config.Options.OutputTarget)
+		var err error
+		tcpConn, err = net.Dial("tcp", config.Options.OutputTarget)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to open tcp connection")
+			os.Exit(1)
+		}
 		fmt.Printf("Output will be redirected to tcp://%s\n", config.Options.OutputTarget)
 	} else if config.Options.Output == "file" {
 		var err error
 		file, err = os.OpenFile(config.Options.OutputTarget, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 		if err != nil {
-			panic(err)
+			fmt.Fprintf(os.Stderr, "failed to open output file")
+			os.Exit(1)
 		}
 		fmt.Printf("Output will be redirected to file://%s\n", config.Options.OutputTarget)
 	} else {
@@ -29,10 +35,15 @@ func outputOpen() {
 
 func output(text string) {
 	if config.Options.Output == "tcp" {
-		fmt.Fprintf(tcpConn, text)
+		_, err := fmt.Fprintf(tcpConn, text)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to send to tcp connection")
+			os.Exit(1)
+		}
 	} else if config.Options.Output == "file" {
 		if _, err := file.WriteString(text); err != nil {
-			panic(err)
+			fmt.Fprintf(os.Stderr, "failed to write to file")
+			os.Exit(1)
 		}
 	} else {
 		// print to stdout
