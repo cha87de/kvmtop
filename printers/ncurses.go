7b952a1cb4a1d227ab99ad4d03637d0c67d8a533
christopher <post@c-ha.de>
2018-07-02 16:49:01 +0200

    improve goncurses ui, add host metrics

--- printers/ncurses.go
+++ printers/ncurses.go
@@ -1,83 +1,65 @@
 package printers
 
 import (
-	"bufio"
-	"bytes"
 	"log"
-
-	tw "text/tabwriter"
+	"sort"
+	"strings"
 
 	"github.com/cha87de/goncurses"
 	"github.com/cha87de/kvmtop/models"
 )
 
-var outputHelpers struct {
-	screen                *goncurses.Window
-	tabwriterBuffer       bytes.Buffer
-	tabwriterBufferWriter bufio.Writer
-	tabwriter             tw.Writer
-}
+var screen *goncurses.Window
 
 // NcursesPrinter describes the ncurses printer
 type NcursesPrinter struct {
 	models.Printer
 }
 
+const HOSTWINHEIGHT = 5
+const DOMAINMAXFIELDWIDTH = 8
+
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+var domainColumnWidths []int
+
 // Open opens the printer
 func (printer *NcursesPrinter) Open() {
 	// Init goncurses
-	s, err := goncurses.Init()
+	var err error
+	screen, err = goncurses.Init()
 	if err != nil {
 		log.Fatal("init", err)
 	}
-	outputHelpers.screen = s
 	goncurses.Echo(false) // turn echoing of typed characters off
 	goncurses.Cursor(0)   // hide cursor
-
-	// declare buffer and writer
-	outputHelpers.tabwriterBuffer = *new(bytes.Buffer)
-	outputHelpers.tabwriterBufferWriter = *bufio.NewWriter(&outputHelpers.tabwriterBuffer)
-	outputHelpers.tabwriter = *new(tw.Writer)
-	outputHelpers.tabwriter.Init(&outputHelpers.tabwriterBufferWriter, 0, 8, 1, ' ', 0)
 }
 
 // Screen prints the measurements on the screen
 func (printer *NcursesPrinter) Screen(printable models.Printable) {
 
-	fields := printable.DomainFields
-	values := printable.DomainValues
+	sortByColumn := 3 // todo: get from input
+	maxy, maxx := screen.MaxYX()
 
-	var buffer bytes.Buffer
-	var rawline string
+	// define host panel
+	hostWin, _ := goncurses.NewWindow(HOSTWINHEIGHT, maxx, 0, 0)
+	// hostWin.Box(goncurses.ACS_VLINE, goncurses.ACS_HLINE)
+	goncurses.UpdatePanels()
+	goncurses.Update()
+	goncurses.NewPanel(hostWin)
+	printHost(hostWin, printable.HostFields, printable.HostValues)
 
-	// write headline
-	for _, field := range fields {
-		buffer.WriteString(field)
-		buffer.WriteString("\t")
-	}
-	buffer.WriteString("\n")
-	rawline = buffer.String()
-	outputHelpers.tabwriter.Write([]byte(rawline))
+	// define domain panel
+	domainWin, _ := goncurses.NewWindow(maxy-HOSTWINHEIGHT, maxx, HOSTWINHEIGHT, 0)
+	// domainWin.Box(goncurses.ACS_VLINE, goncurses.ACS_HLINE)
+	goncurses.UpdatePanels()
+	goncurses.Update()
+	goncurses.NewPanel(domainWin)
+	printDomain(domainWin, printable.DomainFields, printable.DomainValues, sortByColumn)
 
-	// iterate over domains
-	for domvalue := range values {
-		buffer.Reset()
-		for _, value := range values[domvalue] {
-			buffer.WriteString(value)
-			buffer.WriteString("\t")
-		}
-		buffer.WriteString("\n")
-		rawline = buffer.String()
-		outputHelpers.tabwriter.Write([]byte(rawline))
-	}
-
-	outputHelpers.tabwriter.Flush()
-	outputHelpers.tabwriterBufferWriter.Flush()
-	tablines := outputHelpers.tabwriterBuffer.String()
-
-	outputHelpers.screen.MovePrint(0, 0, tablines)
-	outputHelpers.screen.Refresh()
-	outputHelpers.tabwriterBuffer.Reset()
 }
 
 // Close terminates the printer
@@ -88,4 +70,114 @@ func (printer *NcursesPrinter) Close() {
 // CreateNcurses creates a new ncurses printer
 func CreateNcurses() NcursesPrinter {
 	return NcursesPrinter{}
+}
+
+func printHost(window *goncurses.Window, fields []string, values []string) {
+	window.Move(1, 1)
+	// window.Printf("Whatever: %s", values[0])
+	for i, field := range fields {
+		window.Printf("%s: %s, ", field, values[i])
+	}
+}
+
+func printDomain(window *goncurses.Window, fields []string, values map[string][]string, sortByColumn int) {
+	window.Move(1, 1)
+	// write headline
+	currentGroup := ""
+	window.Printf("%s", currentGroup)
+	for columnID, field := range fields {
+		_, cursorPosX := window.CursorYX()
+
+		// split group from field name
+		fieldParts := strings.Split(field, "_")
+		if len(fieldParts) > 1 {
+			groupLabel := strings.Join(fieldParts[0:len(fieldParts)-1], " ")
+			if groupLabel != currentGroup {
+				// print net group label
+				currentGroup = groupLabel
+				window.Move(1, cursorPosX)
+				window.Printf("%s ", groupLabel)
+			}
+		}
+
+		// prepare field label length
+		fieldLabel := prepareForCell(fieldParts[len(fieldParts)-1], columnID)
+
+		// field label bold if sorted by
+		if sortByColumn == columnID {
+			window.AttrOn(goncurses.A_BOLD)
+		} else {
+			window.AttrOff(goncurses.A_BOLD)
+		}
+
+		window.AttrOn(goncurses.A_REVERSE)
+		window.Move(2, cursorPosX)
+		window.Printf("%s ", fieldLabel)
+		window.AttrOff(goncurses.A_REVERSE)
+
+	}
+	window.Println()
+
+	// create ordered domain list
+	domainList := sortDomainIDsByField(values, sortByColumn)
+
+	// iterate over domains
+	rowCounter := 3
+	for _, domain := range domainList {
+		// write domain row
+		window.Move(rowCounter, 1)
+		for columnID, value := range values[domain.Key] {
+			if sortByColumn == columnID {
+				window.AttrOn(goncurses.A_BOLD)
+			} else {
+				window.AttrOff(goncurses.A_BOLD)
+			}
+			value = prepareForCell(value, columnID)
+			window.Printf("%s ", value)
+		}
+		window.Println()
+		rowCounter++
+	}
+}
+
+func sortDomainIDsByField(values map[string][]string, sortByColumn int) []KeyValue {
+	var sorted []KeyValue
+	for key, value := range values {
+		sorted = append(sorted, KeyValue{key, value[sortByColumn]})
+	}
+	sort.Slice(sorted, func(i, j int) bool {
+		return sorted[i].Value > sorted[j].Value
+	})
+	return sorted
+}
+
+func prepareForCell(content string, columnID int) string {
+	return expandCell(fitInCell(content), columnID)
+}
+
+func fitInCell(content string) string {
+	if len(content) > DOMAINMAXFIELDWIDTH {
+		tmp := strings.Split(content, "")
+		content = strings.Join(tmp[0:DOMAINMAXFIELDWIDTH], "")
+	}
+	return content
+}
+
+func expandCell(content string, columnID int) string {
+	if len(domainColumnWidths) <= columnID {
+		// set current width as column width
+		domainColumnWidths = append(domainColumnWidths, len(content))
+	} else if len(content) < domainColumnWidths[columnID] {
+		// column width is larger, expand with spaces
+		spaces := []string{""}
+		diff := domainColumnWidths[columnID] - len(content)
+		for i := 0; i < diff; i++ {
+			spaces = append(spaces, " ")
+		}
+		content = content + strings.Join(spaces, "")
+	} else if len(content) > domainColumnWidths[columnID] {
+		// column width is smaller, store larger width
+		domainColumnWidths[columnID] = len(content)
+	}
+	return content
 }
