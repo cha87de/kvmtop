e06c8afe308b97da24f33117dc6bc7c9826a2061
christopher <post@c-ha.de>
2018-10-02 17:22:15 +0200

    fix net collector: get interfaces from xml file instead of libvirtd

--- collectors/netcollector/domainLookup.go
+++ collectors/netcollector/domainLookup.go
@@ -3,18 +3,32 @@ package netcollector
 import (
 	"github.com/cha87de/kvmtop/models"
 	libvirt "github.com/libvirt/libvirt-go"
+	libvirtxml "github.com/libvirt/libvirt-go-xml"
 )
 
 func domainLookup(domain *models.Domain, libvirtDomain libvirt.Domain) {
 	// get list of network interfaces
-	rawinterfaces, err := libvirtDomain.ListAllInterfaceAddresses(libvirt.DOMAIN_INTERFACE_ADDRESSES_SRC_LEASE)
+	/*rawinterfaces, err := libvirtDomain.ListAllInterfaceAddresses(libvirt.DOMAIN_INTERFACE_ADDRESSES_SRC_LEASE)
 	if err != nil {
 		return
 	}
+	fmt.Printf("rawinterfaces: %+v\n", rawinterfaces)
 	var ifs []string
 	for _, ifx := range rawinterfaces {
 		ifs = append(ifs, ifx.Name)
+	}*/
+
+	var ifs []string
+	xmldoc, _ := libvirtDomain.GetXMLDesc(libvirt.DOMAIN_XML_SECURE)
+	domcfg := &libvirtxml.Domain{}
+	domcfg.Unmarshal(xmldoc)
+
+	for _, devInterface := range domcfg.Devices.Interfaces {
+		if devInterface.Target != nil {
+			ifs = append(ifs, devInterface.Target.Dev)
+		}
 	}
+
 	newMeasurementInterfaces := models.CreateMeasurement(ifs)
 	domain.AddMetricMeasurement("net_interfaces", newMeasurementInterfaces)
 }
