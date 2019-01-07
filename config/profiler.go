9e4b2961a0a4ea50da613aa33324ab64299b4e1e
christopher <post@c-ha.de>
2019-01-07 16:54:14 +0100

    disable profiler filter by default

--- config/profiler.go
+++ config/profiler.go
@@ -7,6 +7,6 @@ type ProfilerOptionsType struct {
 	States        int           `long:"states" default:"4"`
 	BufferSize    int           `long:"buffersize" default:"10"`
 	History       int           `long:"history" default:"1"`
-	FilterStdDevs int           `long:"filterstddevs" default:"2"`
+	FilterStdDevs int           `long:"filterstddevs" default:"-1"`
 	OutputFreq    time.Duration `long:"outputFreq" default:"60"`
 }
